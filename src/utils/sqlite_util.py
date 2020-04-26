53e92d7833a0567dfac73e8ddc56740ba5a47eee
Marcin Wątroba <marcin.watroba@deviniti.com>
2020-04-26 08:43:17 +0200

    Addiotional logs in /get_phrase_tweets

--- src/utils/sqlite_util.py
+++ src/utils/sqlite_util.py
@@ -2,9 +2,17 @@ import sqlite3
 
 import pandas as pd
 
+from utils.docker_logs import get_logger
+
+logger = get_logger('sqlite_util')
+
 
 def get_df_from_sqlite_db(db_filename: str, query: str):
+    logger.info('get_df_from_sqlite_db')
+    logger.info(db_filename)
+    logger.info(query)
     con = sqlite3.connect(db_filename)
     df = pd.read_sql_query(query, con)
+    logger.info(df.size)
     con.close()
     return df
